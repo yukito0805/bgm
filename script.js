a3905888d43617c85693cdad251e7d1284f3e458
yukito0805 <tosiyuki.hama0508@gmail.com>
2025-12-04 13:03:56 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const sound = document.getElementById("sound");
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  sound.currentTime = 0; // 毎回頭から再生
-  sound.play();
-});
